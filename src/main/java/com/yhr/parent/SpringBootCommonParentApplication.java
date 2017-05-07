dcbb286581c711ec8e8b5c54a07e5cc4d45a2baf
yhr <1361579931@qq.com>
2017-05-07 14:14:13 +0800

    提交代码

---
+++ src/main/java/com/yhr/parent/SpringBootCommonParentApplication.java
@@ -0,0 +1,12 @@
+package com.yhr.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCommonParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCommonParentApplication.class, args);
+	}
+}
